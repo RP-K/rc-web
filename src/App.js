dbbaf1fe275fd8dd58884ccd776e4de2b6f1028b
RP <rpmongotest@gmail.com>
2023-02-18 21:25:02 +0530

    Chat page update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router";
+import Footer from "./components/common/Footer";
 import Header from "./components/header";
 
 const App = () => {
   return (
     <div id="main-app">
       <Header />
-      <Outlet />
+      <div className="min-h-screen">
+        <Outlet />
+      </div>
+      <Footer />
     </div>
   );
 };
--- src/components/chat/nav_container/NavArea.js
+++ src/components/chat/nav_container/NavArea.js
@@ -1,14 +1,17 @@
 import { useState } from "react";
 
 const navItems = ["Chats", "Cases", "Contacts"];
-const NavArea = () => {
+const NavArea = ({ show }) => {
   const [activeTab, setActiveTab] = useState("Chats");
   const handleClick = (item) => {
     setActiveTab(item);
   };
   return (
-    <div>
-      <div className="flex gap-x-3 items-center justify-around border-b-[1px] border-b-primary-main">
+    <>
+      <div
+        className="flex gap-x-3 items-center justify-around border-b-[1px] border-b-primary-main transition-all duration-1000"
+        style={{ display: !show && "none" }}
+      >
         {navItems.map((item) => (
           <button
             className={`p-2 transition-all duration-700 ${
@@ -21,14 +24,14 @@ const NavArea = () => {
           </button>
         ))}
       </div>
-      <div className="overflow-y-auto  max-h-[400px]">
+      <div className="overflow-y-auto" style={{ display: !show && "none" }}>
         {[...Array(10).keys()].map((i, j) => (
           <p className="my-10" key={j}>
             {i}
           </p>
         ))}
       </div>
-    </div>
+    </>
   );
 };
 
