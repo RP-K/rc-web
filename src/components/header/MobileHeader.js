dc78516c67180968b64becdd74297f05aa5cc547
krishnakumars59809 <krishnakumars59809work@gmail.com>
2023-02-18 19:34:07 +0530

    Header Update

--- src/components/header/MobileHeader.js
+++ src/components/header/MobileHeader.js
@@ -1,34 +1,51 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "../header/MobileNav.css";
+import ProfileMenu from "./ProfileMenu";
 
-const MobileHeader = () => {
+const MobileHeader = ({ navLinks, logo }) => {
   const [isMobile, setIsMobile] = useState(false);
+  const classNames = (...classes) => {
+    return classes.filter(Boolean).join(" ");
+  };
   return (
     <div>
       {isMobile && (
         <ul className="ul  py-5 space-y-4 font-semibold  bg-primary-main z-40 ">
-          <li>
-            <a to="/">Home</a>
-          </li>
-          <li>
-            {" "}
-            <a to="/">Chat</a>
-          </li>
-          <li>
-            {" "}
-            <a to="/">Help</a>
-          </li>
+          {navLinks.map((link, i) => (
+            <li>
+              {" "}
+              <NavLink
+                key={i}
+                to={link.to}
+                className={({ isActive }) =>
+                  classNames(
+                    isActive &&
+                      "text-secondary-main underline transition-all duration-500",
+                    "font-semibold  py-0.5 items-center"
+                  )
+                }
+              >
+                <div className="">{link.text}</div>
+              </NavLink>
+            </li>
+          ))}
         </ul>
       )}
-
-      <button className="" onClick={() => setIsMobile(!isMobile)}>
-        {isMobile ? (
-          <div className="burger-bar show "></div>
-        ) : (
-          <div className="burger-bar z-50"></div>
-        )}
-      </button>
+      <div className="flex justify-between item-center px-2">
+        <div className="flex justify-start space-x-3">
+          <button className="" onClick={() => setIsMobile(!isMobile)}>
+            {isMobile ? (
+              <div className="burger-bar show "></div>
+            ) : (
+              <div className="burger-bar z-50"></div>
+            )}
+          </button>
+          <img className="h-10" src={logo} alt="Rain Computing" />
+        </div>
+        <ProfileMenu/>
+      </div> 
+      
     </div>
   );
 };
