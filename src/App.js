23457918f88bc9035e817f6a4cd1355295bea07f
gokulgk2000 <110904612+gokulgk2000@users.noreply.github.com>
2023-02-19 00:59:20 +0530

    app.js reworked

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { Outlet } from "react-router";
 import Footer from "./components/common/Footer";
-import Footer from "./components/common/Footer";
 import Header from "./components/header";
 
 const App = () => {
   return (
     <div id="main-app">
       <Header />
+      <div className="min-h-screen my-2">
       <Outlet />
+      </div>
       <Footer/>
     </div>
   );
