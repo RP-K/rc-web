79f0c808b4232b65607c25df6fffe751085384d5
eswaranthangavel23 <aravinthkrish110@gmail.com>
2023-02-21 19:27:02 +0530

    Colour changes

--- src/components/chat/nav_container/index.js
+++ src/components/chat/nav_container/index.js
@@ -14,7 +14,7 @@ const NavContainer = () => {
   }, [isDesktop]);
 
   return (
-    <div className="col-span-2 lg:col-span-1 chat-container shadow rounded-md flex flex-col bg-[#eeeeee]">
+    <div className="col-span-2 lg:col-span-1 chat-container shadow rounded-md flex flex-col bg-[#EFFFFD]">
       <ProfileCard toggleShow={toggleShow} show={show} />
       <NavArea show={show} />
     </div>
